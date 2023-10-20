59cdde9c467e905d39da7a3ab47adbe401a411c1
rsales <rafael.sales93@hotmail.com>
2023-10-19 21:05:32 -0300

    Create Hero component

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav>
+    <div class="container mx-auto py-2 flex flex-wrap items-center md:flex-no-wrap"></div>
+  </nav>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
