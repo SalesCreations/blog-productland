7b08d24bfbd89618d5230e6eb406fbc75bafe044
rsales <rafael.sales93@hotmail.com>
2023-10-19 21:28:04 -0300

    Create Navbar

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="default-layout">
+    <Navbar />
+    <slot />
+
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+const route = useRoute();
+</script>
