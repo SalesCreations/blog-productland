aa1e3c49f35057c3f1d9355c14113c1f8cae0ef6
rsales <rafael.sales93@hotmail.com>
2023-10-28 19:25:48 -0300

    Create Hero component layout.

--- storyblok/Hero.vue
+++ storyblok/Hero.vue
@@ -1,27 +1,49 @@
 <template>
-  <div v-editable="blok" class="bg-white">
-    <div class="px-6 pt-14 lg:px-8">
-      <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+  <div v-editable="blok" id="hero-one" class="bg-gray-50 relative py-32">
+    <div class="container mx-auto px-4 grid gap-5 grid-cols-12" ref="parallaxRef">
+      <img src="@/assets/images/VisualElements.svg" class="w-full absolute right-0 -top-4 z-0" alt="" ref="imageRef" :style="layer0">
+      
+      <div class="hero-content col-start-3 col-span-8 text-center z-10 lg:px-24" ref="textRef" :style="layer1">
+        <h1 class="font-display text-5xl font-black tracking-tight text-gray-900 sm:text-5xl">{{ blok.title }}</h1>
+        <p class="mt-6 text-lg leading-8 text-gray-600">{{ blok.description }}</p>
       </div>
-      <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-          <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
-          </div>
-        </div>
-        <div class="text-center">
-          <h1 class="font-display text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">{{ blok.title }}</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">{{ blok.description }}</p>
-          <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
-          </div>
-        </div>
+
+      <div class="hero-content col-span-12 mt-32 z-20" ref="photoRef" :style="layer2">
+        <img src="@/assets/images/NavbarPhotos@2x.png" class="w-full"  alt="">
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useParallax } from '@vueuse/core'
+
 defineProps({ blok: Object })
+
+const parallaxRef = ref(null)
+// const bgRef = ref(null)
+// const imageRef = ref(null)
+// const textRef = ref(null)
+// const photoRef = ref(null)
+
+const parallax = reactive(useParallax(parallaxRef))
+
+const layer0 = computed(() => ({
+  transform: `translateX(${parallax.tilt * 10}px) translateY(${
+    parallax.roll * 10
+  }px) scale(1.01)`,
+}))
+
+const layer1 = computed(() => ({
+  transform: `translateX(${parallax.tilt * 20}px) translateY(${
+    parallax.roll * 20
+  }px)`,
+}))
+
+const layer2 = computed(() => ({
+  transform: `translateX(${parallax.tilt * 30}px) translateY(${
+    parallax.roll * 30
+  }px)`,
+}))
 </script>
