1cf5b88fe056616f6628c435e19ea420a1fa9b55
rsales <rafael.sales93@hotmail.com>
2023-11-02 16:16:32 -0300

    Update interaction Card components.

--- components/CardArticleDefault.vue
+++ components/CardArticleDefault.vue
@@ -2,11 +2,11 @@
   <div 
     class="card-full relative flex flex-col w-full rounded-xl bg-white bg-clip-border border border-gray-200 text-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer" style="height: 604px;"
   >
-    <div class="relative m-0 w-full h-2/5 shrink-0 overflow-hidden rounded-xl rounded-b-none bg-white bg-clip-border text-gray-700">
+    <div class="relative m-0 w-full h-2/5 shrink-0 overflow-hidden rounded-xl rounded-b-none bg-gray-200 bg-clip-border text-gray-700">
       <img
         src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;"
         alt="image"
-        class="h-full w-full object-cover"
+        class="article-image h-full w-full object-cover"
       />
     </div>
     <div class="p-6 h-full flex">
@@ -26,3 +26,12 @@
     </div>
   </div>
 </template>
+
+<style lang="postcss" scoped>
+  .card-full img.article-image {
+    @apply ease-in-out delay-200 duration-200
+  }
+  .card-full:hover img.article-image {
+    @apply -translate-y-1 scale-105
+  }
+</style>
--- components/CardArticleFull.vue
+++ components/CardArticleFull.vue
@@ -2,11 +2,11 @@
   <div 
     class="card-full relative flex w-full h-96 rounded-xl bg-white bg-clip-border border border-gray-200 text-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer"
   >
-    <div class="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+    <div class="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-gray-200 bg-clip-border text-gray-700">
       <img
         src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&amp;"
         alt="image"
-        class="h-full w-full object-cover"
+        class="article-image h-full w-full object-cover"
       />
     </div>
     <div class="p-6 flex items-center">
@@ -26,3 +26,12 @@
     </div>
   </div>
 </template>
+
+<style lang="postcss" scoped>
+  .card-full img.article-image {
+    @apply ease-in-out delay-200 duration-200
+  }
+  .card-full:hover img.article-image {
+    @apply -translate-y-1 scale-105
+  }
+</style>
