eeee258c543244a20dda4e3af49d2bb200595585
rsales <rafael.sales93@hotmail.com>
2023-11-19 00:30:02 -0300

    Refine event secition design

--- storyblok/EventItem.vue
+++ storyblok/EventItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-editable="blok" class="col-span-12 xl:col-span-10 xl:col-start-2">
     <!-- <pre>{{ blok }}</pre> -->
-    <a href="#" target="_blank">
+    <a :href="blok.link.url" target="_blank">
       <div 
         class="card-full relative flex w-full h-32 rounded-xl bg-white bg-clip-border border border-gray-200 text-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer"
       >
@@ -13,7 +13,11 @@
             <p class="date-time-info text-base">{{ blok.description || "New Event Description" }}</p>
           </div>
         </div>
-        <div class="relative m-0 w-1/4 shrink-0 overflow-hidden rounded-xl rounded-l-none bg-gray-600 bg-clip-border text-gray-700">
+        <div class="relative m-0 w-1/4 shrink-0 overflow-hidden rounded-xl rounded-l-none bg-brand-300 bg-clip-border text-white">
+          <div class="date-info flex flex-col justify-center items-center p-5">
+            <time class="text-3xl font-black">{{ $dayjs(blok.date).format('MMM').toUpperCase()}}</time>
+            <time class="text-5xl font-black">{{ $dayjs(blok.date).format('DD').toUpperCase()}}</time>
+          </div>
         </div>
       </div>
     </a>
