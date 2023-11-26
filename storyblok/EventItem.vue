5d5f78ea4c0b70e6308e277b1290046e5d36f017
rsales <rafael.sales93@hotmail.com>
2023-11-26 13:44:53 -0300

    EventItem component responsiveness

--- storyblok/EventItem.vue
+++ storyblok/EventItem.vue
@@ -2,7 +2,7 @@
   <div v-editable="blok" class="col-span-12 xl:col-span-10 xl:col-start-2">
     <a :href="blok.link.url" target="_blank">
       <div 
-        :class="`card-full relative flex w-full h-32 rounded-xl bg-white bg-clip-border border border-gray-200 text-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer ${new Date($dayjs(blok.date).format('MM-DD-YYYY')) < new Date($dayjs().format('MM-DD-YYYY')) ? 'opacity-50':'opacity-100'}`"
+        :class="`card-full relative flex w-full h-auto sm:h-32 rounded-xl bg-white bg-clip-border border border-gray-200 text-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 cursor-pointer ${new Date($dayjs(blok.date).format('MM-DD-YYYY')) < new Date($dayjs().format('MM-DD-YYYY')) ? 'opacity-50':'opacity-100'}`"
       >
         <div class="p-6 flex items-center grow">
           <div class="article-info">
@@ -13,7 +13,7 @@
           </div>
         </div>
         <div class="relative m-0 w-1/4 shrink-0 overflow-hidden rounded-xl rounded-l-none bg-brand-300 bg-clip-border text-white">
-          <div class="date-info flex flex-col justify-center items-center p-5">
+          <div class="date-info flex flex-col justify-center items-center h-full p-5">
             <time class="text-3xl font-black">{{ $dayjs(blok.date).format('MMM').toUpperCase()}}</time>
             <time class="text-5xl font-black">{{ $dayjs(blok.date).format('DD').toUpperCase()}}</time>
           </div>
