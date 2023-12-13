50deffa1fe0b3ed4010e0418c0eb5f0eb44a3ed6
rsales <rafael.sales93@hotmail.com>
2023-12-12 21:45:18 -0300

    Update meta tags on articles.

--- pages/articles/[slug].vue
+++ pages/articles/[slug].vue
@@ -38,6 +38,10 @@
 </template>
 
 <script setup>
+// =======================
+// initialization variables
+// =======================
+
 const route = useRoute();
 const author = useState();
 const generalContent = useState();
@@ -62,4 +66,42 @@ function readingTime() {
 
   return minutes
 }
+
+// =======================
+// metatags
+// =======================
+
+useHead({
+  title: () => story.value.content.seo?.title,
+  meta: [
+    {
+      name: 'description',
+      content: () => story.value.content.seo?.description,
+    },
+    {
+      property: 'og:site_name',
+      content: 'The Productland',
+    },
+    {
+      property: 'og:title',
+      content: () => story.value.content.seo?.title,
+    },
+    {
+      property: 'og:description',
+      content: () => story.value.content.seo?.description,
+    },
+    {
+      property: 'og:image',
+      content: () => story.value.content.seo?.og_image,
+    },
+    {
+      property: 'twitter:card',
+      content: 'summary_large_image'
+    },
+    {
+      property: 'twitter:image',
+      content: () => story.value.content.seo?.og_image,
+    }
+  ]
+})
 </script>
